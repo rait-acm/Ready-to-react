1b97e2cacc69d41212d8baec1fc674d5b90fde3b
hdck007 <haridarshanofficial@gmail.com>
2021-09-20 13:35:19 +0530

    day1

---
+++ code/script.js
@@ -0,0 +1,70 @@
+// 'use strict'
+//----------------------------------------Day1-----------------------------------------
+// Hello world
+// Line by line execution
+
+// variables, values and types
+// what are values?
+// what are types?
+  // Primitive types
+  // Number
+    // NaN and Infinity
+  // String
+  // Boolean
+  // Symbol
+    // Not of importance as of now
+  // null
+  // undefined
+
+  // non-primitve types
+  // Object
+  //   Function
+  //   Array
+  //   Date
+  //   RegExp
+// what are variables?
+
+
+// declaration of variable using var, let const
+// var
+
+
+// let
+
+
+// const
+
+
+// js is loosely typed language
+
+
+// primitive values are immutable
+
+
+// typeof operator
+
+
+// common arithmetic operator
+// type coercion
+
+
+// Loops for, while, do while
+
+
+// conditionals, if, switch
+
+
+// comparison 
+
+
+// truthy and falsy values
+
+
+// equality of values
+// referencial equality
+
+
+// what is ===?
+
+
+// end
